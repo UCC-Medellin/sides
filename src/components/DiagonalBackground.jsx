491e310de826c59147f2ac4a44fcb44e11543d6f
Juan Sebastian <juan.sebastianri28@gmail.com>
2025-07-25 14:05:10 +0000

    Fix: Add default and miminal clip bg and personalized clip bg to main page

--- src/components/DiagonalBackground.jsx
+++ src/components/DiagonalBackground.jsx
@@ -1,10 +1,10 @@
 import './DiagonalBackground.css'
 import {radialGreenGradient} from '../utils/gradients';
 
-const DiagonalBackground = () => {
+const DiagonalBackground = ({isMain = false}) => {
     return (
         <section
-            className="w-[100dvw] h-[100dvh] absolute top-0 right-0 z-[-100] clip-diagonal"
+            className={"w-[100dvw] h-[100dvh] absolute top-0 right-0 z-[-100] " + (isMain ? "clip-diagonal-main" : "clip-diagonal")}
             style={{ background: radialGreenGradient }}
         >
         </section>
